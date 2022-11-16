b61ba963e993513f883f94d21229afb4b82824c4
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-16 19:21:55 +0100

    rea los métodos get y set para los atributos. Recuerda que para el método set se debe tener en cuenta que los valores indicados sean correctos (las filas van del 1 al 8 -ambos inclusive- y las columnas de la 'a' a la 'h' -ambas inclusive-) y si no se lance una excepción del tipo IllegalArgumentException con el mensaje adecuado. En caso de que la posición no sea la correcta no deben modificarse los atributos. Realiza un commit.
    Como sabiamos la cantidad de variables que habria en el char de el
    setColumna, hemos decidido hacerlo con un switch, dejando como default
    el error porque no ería ninguna de las otras opciones.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -1,5 +1,66 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 public class Posicion {
+	private int fila;
+
+	private char columna;
+
+	public int getFila() {
+		return fila;
+	}
+
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("El valor de la fila es incorrecto.");
+		} else {
+			this.fila = fila;
+		}
+
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+		switch(columna) {
+		
+		case 'A':
+			this.columna = columna;
+			break;
+		
+		case 'B':
+			this.columna = columna;
+			break;
+		
+		case 'C':
+			this.columna = columna;
+			break;
+		
+		case 'D':
+			this.columna = columna;
+			break;
+			
+		case 'E':
+			this.columna = columna;
+			break;
+			
+		case 'F':
+			this.columna = columna;
+			break;
+			
+		case 'G':
+			this.columna = columna;
+			break;
+			
+		case 'H':
+			this.columna = columna;
+			break;
+			
+		default:
+			throw new IllegalArgumentException("El valor de la columna es incorrecto.");
+		}
+		
+	}
 
 }
