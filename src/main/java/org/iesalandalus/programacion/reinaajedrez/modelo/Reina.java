0815956c178b57046f16aef92c2a058a58d975da
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-16 23:58:46 +0100

    Crea los métodos get y set para cada atributo con la visibilidad adecuada. Los métodos set siempre comprobarán la validez de los parámetros pasados y si no son correctos deberá lanzar la excepción adecuada con el mensaje adecuado. Realiza un commit.
    Debido a que hice el getColumna como un switch, he tenido que hacer que
    compruebe cada letra en una gran cadena de ||.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -4,12 +4,48 @@ public class Reina {
 
 	private Color color;
 	private Posicion posicion;
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public Reina(Reina reina) {
+
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		if (color == Color.BLANCO || color == Color.NEGRO) {
+
+			this.color = color;
+		} else {
+			throw new IllegalArgumentException("ERROR: Color no válida.");
+
+		}
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+
+		if (posicion.getFila() >= 1 && posicion.getFila() <= 8 &&
+
+				(
+				posicion.getColumna() == 'a' ||
+				posicion.getColumna() == 'b' ||
+				posicion.getColumna() == 'c' ||
+				posicion.getColumna() == 'd' ||
+				posicion.getColumna() == 'e' ||
+				posicion.getColumna() == 'f' ||
+				posicion.getColumna() == 'g' ||
+				posicion.getColumna() == 'h'))
+
+		{
+			this.posicion = posicion;
+
+		} else {
+			throw new IllegalArgumentException("ERROR: Posición no válida.");
+		}
+	}
 }
