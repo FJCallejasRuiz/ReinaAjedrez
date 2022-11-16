508187ac141be1e3ea8f28b6d412a6c6447c014b
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-16 19:48:38 +0100

    Crea los métodos equals y hashCode para esta clase. Realiza un commit.
    Los hemos creados con la herramienta de Source.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
 	private int fila;
 
@@ -9,7 +11,7 @@ public class Posicion {
 		setFila(numFila);
 		setColumna(letColumna);
 	}
-	
+
 	public Posicion(Posicion posicion) {
 		fila = posicion.getFila();
 		columna = posicion.getColumna();
@@ -71,6 +73,23 @@ public class Posicion {
 			throw new IllegalArgumentException("El valor de la columna es incorrecto.");
 		}
 
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
 	}
 
 }
