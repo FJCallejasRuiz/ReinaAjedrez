d9438a5a78de23d933c51afaa20786c8234485bc
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-16 18:47:31 +0100

    Crea el método toString que devolverá la representación en forma de cadena del literal. Realiza un commit.
    	Hemos tenido complicaciones a la hora de rellenar el toString, pero
    hemos logrado solo con el "return cadenaAMostrar" que pase el test.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -3,13 +3,18 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 public enum Color {
 	BLANCO("Blanco"),
 	NEGRO("Negro");
-
+	
 	public String cadenaAMostrar;
 
 	private Color(String cadena) {
 		this.cadenaAMostrar = cadena;
 	}
 	
+@Override
+public String toString() {
+	return cadenaAMostrar;
+	
+	}
 	
 	
 }
