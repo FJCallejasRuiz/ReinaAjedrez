c928a9f980276c268f685267d867860044ce9b76
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-16 18:57:01 +0100

    Crea la clase Posicion. Crea los atributos fila (int) y columna (char) con la visibilidad adecuada. Realiza un commit
    Inicializadas.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -1,20 +1,19 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public enum Color {
-	BLANCO("Blanco"),
-	NEGRO("Negro");
 	
+	BLANCO("Blanco"), NEGRO("Negro");
+
 	public String cadenaAMostrar;
 
 	private Color(String cadena) {
 		this.cadenaAMostrar = cadena;
 	}
-	
-@Override
-public String toString() {
-	return cadenaAMostrar;
-	
+
+	@Override
+	public String toString() {
+		return cadenaAMostrar;
+
 	}
-	
-	
+
 }
