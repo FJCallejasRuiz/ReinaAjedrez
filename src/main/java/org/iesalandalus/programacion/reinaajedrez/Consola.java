fedeb71c46008560b88e5440b3fb0876cc1710e2
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-17 03:40:31 +0100

    Crea el método elegirOpcionMenu que mostrará un mensaje para que elijamos una opción del menú anteriormente creado y nos pedirá que introduzcamos por teclado la opción hasta que ésta sea valida. Devolverá la opción elegida. Realiza un commit.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,31 +1,45 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
-	
+
 	private Consola() {
 	}
-	
-	public static void mostrarMenu () {
+
+	public static void mostrarMenu() {
 		System.out.println("Menú del ajedrez realizado por Francisco Javier Callejas Ruiz.");
-		System.out.println("Por favor, escriba el número de la opción que desee.");
 		System.out.println("1. Crear reina por defecto.");
-		System.out.println("2. Crear reina eligiendo un color");
+		System.out.println("2. Crear reina eligiendo un color.");
 		System.out.println("3. Mover reina.");
-		System.out.println("4. Salir del menú");
+		System.out.println("4. Salir del menú.");
+	}
+
+	public static int elegirOpcionMenu() {
+		System.out.println("Por favor, escriba el número de la opción que desee.");
+		int numeroMenu = Entrada.entero();
+		while (numeroMenu < 1 || numeroMenu > 4) {
+			System.out.println("El número introducido no pertenece a una opción, inténtelo de nuevo.");
+			numeroMenu = Entrada.entero();
 		}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		switch (numeroMenu) {
+
+		case 1:
+			System.out.println("Has escogido la opción '1. Crear reina por defecto.'");
+
+			break;
+		case 2:
+			System.out.println("Has escogido la opción '2. Crear reina eligiendo un color.'");
+			break;
+		case 3:
+			System.out.println("Has escogido la opción '3. Mover reina.'");
+			break;
+		case 4:
+			System.out.println("Has escogido la opción '4. Salir del menú.'");
+			break;
+		}
+		return numeroMenu;
+
+	}
+
 }
