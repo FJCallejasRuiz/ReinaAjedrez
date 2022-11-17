88f74fb7dd9621ac276aa153577335d6da241241
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-17 12:58:43 +0100

    Crea el método mostrarMenuDirecciones que mostrará por consola un menú con las diferentes direcciones que podemos elegir. Realiza un commit.
    He tenido que procesar que claro, el switch necesita un caso default
    porque el no sabe que si o si será uno de los case, asi que como default
    he puesto la opcion NOROESTE porque si no es del 1-7, es si o si 8.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,6 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Direccion;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
@@ -40,25 +41,60 @@ public class Consola {
 			break;
 		}
 		return numeroMenu;
-		}
+	}
 
 	public static Color elegirOpcion() {
 		System.out.println("Elija el color de la reina.");
 		System.out.println("1. BLANCA.");
 		System.out.println("2. NEGRA.");
-		int colorMenu=Entrada.entero();
-		while (colorMenu!=1 || colorMenu!=2) {
+		int colorMenu = Entrada.entero();
+		while (colorMenu != 1 || colorMenu != 2) {
 			System.out.println("El número introducido no pertenece a una opción, inténtelo de nuevo.");
 			colorMenu = Entrada.entero();
 		}
-		if(colorMenu==1) {
+		if (colorMenu == 1) {
 			System.out.println("Has escogido la opción '1. BLANCA.'");
 			return Color.BLANCO;
-		}else
+		} else
 			System.out.println("Has escogido la opción '2. NEGRA.'");
-			return Color.NEGRO;
+		return Color.NEGRO;
 	}
-		
+
+	public static Direccion mostrarMenuDirecciones() {
+		System.out.println("Elija la dirección.");
+		System.out.println("1. Norte.");
+		System.out.println("2. Noreste.");
+		System.out.println("3. Este.");
+		System.out.println("4. Sureste.");
+		System.out.println("5. Sur.");
+		System.out.println("6. Suroeste.");
+		System.out.println("7. Oeste.");
+		System.out.println("8. Noroeste.");
+		int direccionMenu = Entrada.entero();
+		while (direccionMenu < 1 || direccionMenu > 8) {
+			System.out.println("El número introducido no pertenece a una opción, inténtelo de nuevo.");
+			direccionMenu = Entrada.entero();
+		}
+		switch (direccionMenu) {
+		case 1:
+			return Direccion.NORTE;
+		case 2:
+			return Direccion.NORESTE;
+		case 3:
+			return Direccion.ESTE;
+		case 4:
+			return Direccion.SURESTE;
+		case 5:
+			return Direccion.SUR;
+		case 6:
+			return Direccion.SUROESTE;
+		case 7:
+			return Direccion.OESTE;
+		default:
+			return Direccion.NOROESTE;
+		}
+
+	}
 	
 	
 	
@@ -66,23 +102,5 @@ public class Consola {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
