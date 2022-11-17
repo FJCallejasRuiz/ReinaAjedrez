6ac154e9100dbac918cf81c21765fdfdfe19837f
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-17 02:04:23 +0100

    Crea el método mover que acepte como parámetro una Direccion y la cantidad de pasos a mover en dicha dirección. Los pasos deben estar comprendidos entre 1 y 7 (ambos inclusive) y la dirección no puede ser nula o de lo contrario debe lanzar una excepción adecuada (NullPointerException o IllegalArgumentException) con el mensaje adecuado. Si no puede realizar dicho movimiento, debido a que la reina se sale del tablero, se debe lanzar una excepción del tipo OperationNotSupportedException con un mensaje adecuado y no modificará la posición de la reina. Realiza un commit.
    Gracias a otros metodos y constructores he conseguido hacerlo con más
    facilidad, la mayor cuestion ha sido crear unos números temporales para
    guardar las sumas o restas para la direccion.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Reina {
 
 	private Color color;
@@ -61,7 +63,71 @@ public class Reina {
 		}
 	}
 	
-	
+	public void mover (Direccion direccion, int pasos) throws OperationNotSupportedException {
+		if (direccion == null) {
+			throw new NullPointerException("ERROR: No es posible una dirección nula.");
+		}
+		
+		if (pasos<1 || pasos>7) {
+			throw new IllegalArgumentException("ERROR: Número no válidos.");
+		}
+		
+		int filaTemporal=posicion.getFila();
+		char colTemporal=posicion.getColumna();
+		
+		switch (direccion) {
+			case NORTE:	
+				filaTemporal += pasos;
+			break;
+			
+			case NORESTE:
+				filaTemporal += pasos;
+				colTemporal += pasos;
+			break;
+			
+			case ESTE:
+				colTemporal += pasos;
+			break;
+			
+			case SURESTE:
+				filaTemporal -= pasos;
+				colTemporal += pasos;
+			break;
+			
+			case SUR:
+				filaTemporal -= pasos;
+			break;
+			
+			case SUROESTE:
+				filaTemporal -= pasos;
+				colTemporal -= pasos;
+			break;
+			
+			case OESTE:
+				colTemporal += pasos;
+			break;
+			
+			case NOROESTE:
+				filaTemporal += pasos;
+				colTemporal += pasos;
+			break;	
+		}
+		if (filaTemporal<1 || filaTemporal>8 || (
+				colTemporal != 'a' ||
+				colTemporal != 'b' ||
+				colTemporal != 'c' ||
+				colTemporal != 'd' ||
+				colTemporal != 'e' ||	
+				colTemporal != 'f' ||
+				colTemporal != 'g' ||
+				colTemporal != 'h')) {
+			throw new OperationNotSupportedException("ERROR: No es posible que la reina esté fuera del tablero.");
+			
+		}else {
+			posicion=new Posicion(filaTemporal,colTemporal);
+		}
+		
+	}
 	
 	
 	
