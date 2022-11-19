0b846c8012863aeeeb791ea0a8517d63542ca961
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-19 17:57:09 +0100

    Crea el método crearReinaColor que asignará al atributo de clase reina una nueva instancia de una reina creada con el constructor al que le pasamos el color. Realiza un commit.
    Sacamos los metodos anteriores del main que sino estará apareciendo todo
    el rato y creamos el metodo para crear reina de color.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,39 +1,39 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class MainApp {
-	
+
 	private static void crearReinaDefecto() {
-		Reina reina=new Reina();
+		Reina reina = new Reina();
 	}
-	
-	Reina reina=null;
 
-	public static void main(String[] args) {
+	private static void crearReinaColor(Color color) {
 
-		
-		
-		int opcion=Consola.elegirOpcionMenu();
-		
-		
+		Reina reina = new Reina(color);
+
+	}
+
+	int opcion = Consola.elegirOpcionMenu();
+	{
 
 		switch (opcion) {
-		
+
 		case 1:
-			
-			
+
 		case 2:
-			
+
 		case 3:
-			
-		default:	
-		
-		
+
+		default:
 		}
-		
-		
-		
 	}
+	
+	public Reina reina = null;
+	
+	public static void main(String[] args) {
+	}
+
 }
