f372fa8532fcef27f45ef89db0256ccb3c96c399
fran_ <fran_@LAPTOP-FO9M2E4T>
2022-11-19 17:43:04 +0100

    Crea el método crearReinaDefecto que asignará al atributo de clase reina una nueva instancia de una reina creada con el constructor por defecto. Realiza un commit.
    Si ahcemos ctrl+click al Reina podemos ver que enlaza bien.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -4,16 +4,25 @@ import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class MainApp {
+	
+	private static void crearReinaDefecto() {
+		Reina reina=new Reina();
+	}
+	
+	Reina reina=null;
 
 	public static void main(String[] args) {
 
-		Reina reina=null;
+		
 		
 		int opcion=Consola.elegirOpcionMenu();
+		
+		
 
 		switch (opcion) {
 		
 		case 1:
+			
 			
 		case 2:
 			
